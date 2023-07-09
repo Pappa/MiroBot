0d01e925dcbad058768b6543801d425c004e1599
Pappa <dr.pappa@gmail.com>
2023-07-09 11:28:32 +0100

    Notebooks

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -30,3 +30,27 @@ def display(
         print(f"\nSaved to {save_to}")
 
     plt.show()
+
+
+
+def display_rows(
+    images, size=(12, 6), r=3, c=6, cmap="gray_r", as_type="float32", save_to=None
+):
+    """
+    Displays n random images from each one of the supplied arrays.
+    """
+    if images.max() > 1.0:
+        images = images / 255.0
+    elif images.min() < 0.0:
+        images = (images + 1.0) / 2.0
+
+    fig, axs = plt.subplots(r, c, figsize=size)
+
+    cnt = 0
+    for i in range(r):
+        for j in range(c):
+            axs[i, j].imshow(images[cnt], cmap=None)
+            axs[i, j].axis("off")
+            cnt += 1
+
+    plt.show()
